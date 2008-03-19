1627f3465022b7501bd1432c591282294c1a1b95
Juan Francisco Cantero Hurtado <juanfra684@gmail.com>
2008-03-19 19:10:12 +0100

    Modificados los permisos de casi todos los archivos. Estaban mal y por eso "make install" no funcionaba correctamente. Se cambia "target" a minúsculas (en la parte de la instalación) en gr-lida.pro para ajustarlo a las indicaciones de la documentación de qmake. Se cambia "funciones.h" para la próxima versión.

--- src/funciones.h
+++ src/funciones.h
@@ -35,7 +35,7 @@ public:
 	~Funciones();
 	
 // Versión del GR-lida
-	QString stVersionGrl(){ return "0.3.2"; }
+	QString stVersionGrl(){ return "0.3.4"; }
 // Versiones que soporta
 	QString stVersionDbx(){ return "0.72";  }
 	QString stVersionSvm(){ return "0.10.0";}
