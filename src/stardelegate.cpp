2dfdc12cf7d1a9a7fc7406c9af29ea601a12ada3
Monthy <ermonthy@gmail.com>
2008-08-31 21:07:55 +0200

    Corregido pequeño error al dibujar el rating, ya lo hacia también en zonas que no tenia que hacerlo.

--- src/stardelegate.cpp
+++ src/stardelegate.cpp
@@ -23,19 +23,22 @@ void StarDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option,
 		if (option.state & QStyle::State_Selected)
 			painter->fillRect(option.rect, option.palette.highlight() );
 
-		int rating = model->data(index, Qt::DisplayRole).toInt();
-		int width  = star_on.width();
-		int height = star_on.height();
-		int x = option.rect.x();
-		int y = option.rect.y() + (option.rect.height() / 2) - (height / 2);
-		for (int i = 0; i < 5; ++i) {
-			if(i < rating)
-				painter->drawPixmap(x, y, star_on );
-			else
-				painter->drawPixmap(x, y, star_off );
-			x += width;
+		if( model->data(index, Qt::DisplayRole).toString()!="")
+		{
+			int rating = model->data(index, Qt::DisplayRole).toInt();
+			int width  = star_on.width();
+			int height = star_on.height();
+			int x = option.rect.x();
+			int y = option.rect.y() + (option.rect.height() / 2) - (height / 2);
+			for (int i = 0; i < 5; ++i)
+			{
+				if(i < rating)
+					painter->drawPixmap(x, y, star_on );
+				else
+					painter->drawPixmap(x, y, star_off );
+				x += width;
+			}
 		}
-
 		drawFocus(painter, option, option.rect.adjusted(0, 0, 0, 0));
 	} else {
 		QItemDelegate::paint(painter, option, index);
@@ -60,16 +63,17 @@ bool StarDelegate::editorEvent(QEvent *event, QAbstractItemModel *model,
 	if (event->type() == QEvent::MouseButtonPress)
 	{
 		QMouseEvent *mouseEvent = static_cast<QMouseEvent*>(event);
-		int stars = qBound(0, int(0.7 + qreal(mouseEvent->pos().x()
-			- option.rect.x()) / star_on.width()), 5);
+		if (model->data(index, Qt::DisplayRole).toString()!="")
+		{
+			int stars = qBound(0, int(0.7 + qreal(mouseEvent->pos().x()
+				- option.rect.x()) / star_on.width()), 5);
 
-		model->setData(index, QVariant(stars));
+			model->setData(index, QVariant(stars));
 
-		QString id_grl = model->data(index.sibling(index.row(),0), Qt::DisplayRole).toString();
-		sql->ItemActualizaDatosRating( QVariant(stars).toString(), id_grl );
-
+			QString id_grl = model->data(index.sibling(index.row(),0), Qt::DisplayRole).toString();
+			sql->ItemActualizaDatosRating( QVariant(stars).toString(), id_grl );
+		}
 		return false; //so that the selection can change
 	}
-
 	return true;
 }
